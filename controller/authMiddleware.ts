9ff3cef0c8e9df269edf5bdd85d03c609a79c643
esckles <davidsamzzywilliams1234@gmail.com>
2025-07-14 01:06:38 +0100

    d

--- controller/authMiddleware.ts
+++ controller/authMiddleware.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { Request, Response, NextFunction } from "express";
+import { Response, NextFunction } from "express";
 
 export const verifyToken = (req: any, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(" ")[1];
